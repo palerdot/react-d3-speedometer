ae7386bdfe25c9b66f2eabe3bbdf8c8e3675db58
Arun <palerdot@gmail.com>
2020-03-21 22:26:35 +0530

    [tests] custom segment colors

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -313,22 +313,6 @@ describe("<ReactSpeedometer />", () => {
     )
   })
 
-  test("custom segment colors", () => {
-    const segmentColors = ["red", "blue", "green"]
-    const full_dom_wrapper = mount(
-      <ReactSpeedometer segments={3} segmentColors={segmentColors} />
-    )
-
-    segmentColors.forEach((color, index) => {
-      expect(
-        full_dom_wrapper
-          .render()
-          .find("path.speedo-segment")
-          .get(index).attribs.fill
-      ).toEqual(color)
-    })
-  })
-
   test("label and value font sizes", () => {
     const labelFontSize = "15px"
     const valueTextFontSize = "23px"
@@ -374,5 +358,77 @@ describe("<ReactSpeedometer />", () => {
     })
 
     expect(styles["font-size"]).toEqual(valueTextFontSize)
+  })
+})
+
+describe("Custom Segment Colors", () => {
+  test("custom segment colors works as expected", () => {
+    const segmentColors = ["red", "blue", "green"]
+    const full_dom_wrapper = mount(
+      <ReactSpeedometer segments={3} segmentColors={segmentColors} />
+    )
+
+    segmentColors.forEach((color, index) => {
+      expect(
+        full_dom_wrapper
+          .render()
+          .find("path.speedo-segment")
+          .get(index).attribs.fill
+      ).toEqual(color)
+    })
+  })
+
+  test("6 custom segment colors", () => {
+    const segmentColors = [
+      "#e60000",
+      "#e67300",
+      "#e6e600",
+      "#bcf5bc",
+      "#228b22",
+      "#ff6347",
+    ]
+    const full_dom_wrapper = mount(
+      <ReactSpeedometer
+        segments={6}
+        segmentColors={segmentColors}
+        minValue={0}
+        maxValue={10}
+        value={10}
+        currentValueText={`1.5%`}
+        height={200}
+      />
+    )
+
+    segmentColors.forEach((color, index) => {
+      expect(
+        full_dom_wrapper
+          .render()
+          .find("path.speedo-segment")
+          .get(index).attribs.fill
+      ).toEqual(color)
+    })
+  })
+
+  test("custom segment colors with custom segment stops ", () => {
+    const segmentColors = ["firebrick", "tomato", "gold", "limegreen"]
+    const full_dom_wrapper = mount(
+      <ReactSpeedometer
+        needleHeightRatio={0.7}
+        maxSegmentLabels={5}
+        segments={3}
+        customSegmentStops={[0, 500, 750, 900, 1000]}
+        segmentColors={segmentColors}
+        value={333}
+      />
+    )
+
+    segmentColors.forEach((color, index) => {
+      expect(
+        full_dom_wrapper
+          .render()
+          .find("path.speedo-segment")
+          .get(index).attribs.fill
+      ).toEqual(color)
+    })
   })
 })
