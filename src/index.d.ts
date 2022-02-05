f6398abcd918b325657e0491fb9dd198745b86c3
palerdot <palerdot@gmail.com>
2022-02-05 13:05:31 +0530

    fix(types): svgAriaLabel prop TS types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,39 +1,39 @@
-declare module "react-d3-speedometer" {
+declare module 'react-d3-speedometer' {
   enum Transition {
-    easeLinear = "easeLinear",
-    easeQuadIn = "easeQuadIn",
-    easeQuadOut = "easeQuadOut",
-    easeQuadInOut = "easeQuadInOut",
-    easeCubicIn = "easeCubicIn",
-    easeCubicOut = "easeCubicOut",
-    easeCubicInOut = "easeCubicInOut",
-    easePolyIn = "easePolyIn",
-    easePolyOut = "easePolyOut",
-    easePolyInOut = "easePolyInOut",
-    easeSinIn = "easeSinIn",
-    easeSinOut = "easeSinOut",
-    easeSinInOut = "easeSinInOut",
-    easeExpIn = "easeExpIn",
-    easeExpOut = "easeExpOut",
-    easeExpInOut = "easeExpInOut",
-    easeCircleIn = "easeCircleIn",
-    easeCircleOut = "easeCircleOut",
-    easeCircleInOut = "easeCircleInOut",
-    easeBounceIn = "easeBounceIn",
-    easeBounceOut = "easeBounceOut",
-    easeBounceInOut = "easeBounceInOut",
-    easeBackIn = "easeBackIn",
-    easeBackOut = "easeBackOut",
-    easeBackInOut = "easeBackInOut",
-    easeElasticIn = "easeElasticIn",
-    easeElasticOut = "easeElasticOut",
-    easeElasticInOut = "easeElasticInOut",
-    easeElastic = "easeElastic",
+    easeLinear = 'easeLinear',
+    easeQuadIn = 'easeQuadIn',
+    easeQuadOut = 'easeQuadOut',
+    easeQuadInOut = 'easeQuadInOut',
+    easeCubicIn = 'easeCubicIn',
+    easeCubicOut = 'easeCubicOut',
+    easeCubicInOut = 'easeCubicInOut',
+    easePolyIn = 'easePolyIn',
+    easePolyOut = 'easePolyOut',
+    easePolyInOut = 'easePolyInOut',
+    easeSinIn = 'easeSinIn',
+    easeSinOut = 'easeSinOut',
+    easeSinInOut = 'easeSinInOut',
+    easeExpIn = 'easeExpIn',
+    easeExpOut = 'easeExpOut',
+    easeExpInOut = 'easeExpInOut',
+    easeCircleIn = 'easeCircleIn',
+    easeCircleOut = 'easeCircleOut',
+    easeCircleInOut = 'easeCircleInOut',
+    easeBounceIn = 'easeBounceIn',
+    easeBounceOut = 'easeBounceOut',
+    easeBounceInOut = 'easeBounceInOut',
+    easeBackIn = 'easeBackIn',
+    easeBackOut = 'easeBackOut',
+    easeBackInOut = 'easeBackInOut',
+    easeElasticIn = 'easeElasticIn',
+    easeElasticOut = 'easeElasticOut',
+    easeElasticInOut = 'easeElasticInOut',
+    easeElastic = 'easeElastic',
   }
 
   enum CustomSegmentLabelPosition {
-    Outside = "OUTSIDE",
-    Inside = "INSIDE",
+    Outside = 'OUTSIDE',
+    Inside = 'INSIDE',
   }
 
   type CustomSegmentLabel = {
@@ -86,6 +86,9 @@ declare module "react-d3-speedometer" {
 
     paddingHorizontal?: number
     paddingVertical?: number
+
+    // Accessibility releated props
+    svgAriaLabel?: string
   }
 
   const ReactSpeedometer: React.FunctionComponent<Props>
