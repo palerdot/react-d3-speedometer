33d5daeb632b7769361cb520eaa0b60edbf898f5
palerdot <palerdot@gmail.com>
2022-09-28 20:18:54 +0530

    fix theming brand title

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -4,9 +4,13 @@ import { themes, create } from '@storybook/theming'
 import speedoTheme from '../src/core/theme'
 
 const theme = create(speedoTheme)
+const finalTheme = {
+  ...themes.dark,
+  ...theme,
+}
 
 addons.setConfig({
   showPanel: true,
   // theme: themes.dark,
-  theme,
+  theme: finalTheme,
 })
--- src/core/theme/index.js
+++ src/core/theme/index.js
@@ -4,6 +4,10 @@ const theme = {
   appBg: '#413c69',
   appContentBg: '#2a2744',
   barBg: '#373359',
+
+  brandTitle: 'react-d3-speedometer',
+  brandUrl: 'https://github.com/palerdot/react-d3-speedometer',
+  // brandTarget: '_self',
 }
 
 export default theme
