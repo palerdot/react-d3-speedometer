929b661c0c4cf43b141ec006781f964bd42a7fe1
palerdot <palerdot@gmail.com>
2022-09-28 20:02:41 +0530

    add custom theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,12 @@
-import { addons } from "@storybook/addons"
-import { themes } from "@storybook/theming"
+import { addons } from '@storybook/addons'
+import { themes, create } from '@storybook/theming'
+
+import speedoTheme from '../src/core/theme'
+
+const theme = create(speedoTheme)
 
 addons.setConfig({
   showPanel: true,
-  theme: themes.dark,
+  // theme: themes.dark,
+  theme,
 })
