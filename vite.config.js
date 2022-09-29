eb613b8e740d6157a8d38bdf5e40bd6bdc9addaf
palerdot <palerdot@gmail.com>
2022-09-29 11:06:59 +0530

    vite: externalize window for rollup

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,7 @@ module.exports = defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'window'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
